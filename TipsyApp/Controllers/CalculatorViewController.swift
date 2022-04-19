4e2c6ac133f0ab2e7cc250a377890a897e97ad8b
Forddev777 <taothongdev@gmail.com>
2022-04-19 23:25:30 +0700

    TipsApp
    TipsApp

--- TipsyApp/Controllers/CalculatorViewController.swift
+++ TipsyApp/Controllers/CalculatorViewController.swift
@@ -14,21 +14,53 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
-    
-    
-
+  
+  
+    var tip = 0.10       // start  tip
+    var numberOfPeople = 2  // numofpeople
+    var sumbill = 0.0    // sum bill
+    var finalResult = "0.0"   // result_total
     @IBAction func tipChanged(_ sender: UIButton) {
-        zeroPctButton.isSelected = true
+       
+        
+        billTextField.endEditing(true)        // textfextfield == true
+        zeroPctButton.isSelected = false      //  ถ้ายังไม่ได้เลือกให้ค่าเป็น false
+        tenPctButton.isSelected = false       //  ถ้ายังไม่ได้เลือกให้ค่าเป็น false
+        twentyPctButton.isSelected = false    //  ถ้ายังไม่ได้เลือกให้ค่าเป็น false
+        sender.isSelected = true              //  ถ้าเลือกให้ค่าเป็น true
+        let buttonTitle = sender.currentTitle!    // เก็บค่า button ไว้ใน buttonTitle
+        let buttonTitleMinusPercentSign =  String(buttonTitle.dropLast()) // นำค่ามาเก็บไว้ collertion
+        let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
+        tip = buttonTitleAsANumber / 100
+            // นำค่าที่ได้ มา / 100
     }
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+        splitNumberLabel.text = String(format: "%.0f", sender.value)  // เก็บค่าเลขซ้ายมือ จาก sender.value
+        numberOfPeople = Int(sender.value)  // จำนวนคนที่ต้องการให้  tip
     }
     
+    // เมื่อกด btn Calculate  เข้า  function calculatePressed //
     @IBAction func calculatePressed(_ sender: UIButton) {
+         let bill = billTextField.text!   // check ว่า billtext != " "
+        if bill != "" {
+                sumbill = Double(bill)!
+            let result = sumbill * (1 + tip) / Double(numberOfPeople)
+            finalResult = String(format: "%.2f", result)
         
-       
-          let data = self.twentyPctButton.currentTitle
-        print(data)
+        }
+        // หลังจาก กด  btn Calculate ถ้ามีการ พูกหน้าไว้ ระบุ withIdentifier : " "
+       performSegue(withIdentifier: "gotoCal", sender: self)
+    }
+     // Story board segue ทำงาน
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
+        if segue.identifier == "gotoCal"{
+                let dataVC =  segue.destination as! ResultsViewController
+                    dataVC.result = finalResult
+                    dataVC.tip = Int(tip * 100)
+                    dataVC.split = numberOfPeople
+        }
     }
     
 }
--- TipsyApp/Controllers/ResultsViewController.swift
+++ TipsyApp/Controllers/ResultsViewController.swift
@@ -14,13 +14,20 @@ class ResultsViewController: UIViewController {
     @IBOutlet weak var settingLabel: UILabel!
     
     
+    var result = "0.0"  // ค่าเริ่มต้น  = 0
+    var tip = 10    // default = 10
+    var split = 2   // default = 2 pelope
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+         
+        tatalLabel.text = result
+        settingLabel.text =  "Split between \(split) people, with \(tip)% tip."
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+        
     }
 }
